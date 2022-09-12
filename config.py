15e9b0bdc9be08c5f6e2cd4a3e5d3bf58643b0c0
Laurencemark <112898928+Laurencemark@users.noreply.github.com>
2022-09-12 12:31:24 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2022 laurence mark
+
+import os
+
+class Config(object):
+    APP_ID = int(os.environ.get("APP_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    OWNER_ID = int(os.environ.get("OWNER_ID"))
+    # Chat bot's name
+    CHAT_BOT_NAME = os.environ.get("CHAT_BOT_NAME", "Mark AI")
+    # Your OpenAI API key
+    OPENAI_KEY = os.environ.get("OPENAI_KEY")
+    # Your ARQ API Key
+    ARQ_API_URL = "http://arq.hamker.dev"
+    ARQ_KEY = os.environ.get("ARQ_KEY")
+    # Default Chatbot engine you want to use after OpenAI
+    DEFAULT_CHATBOT = os.environ.get("DEFAULT_CHATBOT", "affiliateplus")
+    # Set ON_HEROKU to False if you aren't on heroku
+    ON_HEROKU = bool(os.environ.get("ON_HEROKU", False))
+    HEROKU_API = os.environ.get("HEROKU_API")
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
