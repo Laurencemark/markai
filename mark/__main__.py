cca1488a0cb2a6cd04190a70859d847cb364d49a
Laurencemark <112898928+Laurencemark@users.noreply.github.com>
2022-09-12 12:39:08 +0530

    Add files via upload

---
+++ mark/__main__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022 laurence mark
+
+from pyrogram import idle
+from . import markai
+
+if __name__ == "__main__" :
+    yuiai.start()
+    print("Mark is live! Join @i_laurencemark")
+    idle()
